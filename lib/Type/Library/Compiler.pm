838883d366c62e8f2638a27fb0bda215f1b04abe
Toby Inkster <mail@tobyinkster.co.uk>
2022-07-13 12:08:52 +0100

    bump version; changelog

--- lib/Type/Library/Compiler.pm
+++ lib/Type/Library/Compiler.pm
@@ -5,7 +5,7 @@ use warnings;
 package Type::Library::Compiler;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.003';
+our $VERSION   = '0.004';
 
 use Type::Library::Compiler::Mite -all;
 use B ();
